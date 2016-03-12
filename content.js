c22ec0347e28092da78afe8a38699e7a411c2364
Danny Liu <dannykliu@gmail.com>
2016-03-12 15:54:47 -0500

    fix dictionary, everything works

--- content.js
+++ content.js
@@ -1,53 +1,60 @@
-var token;
-var tags=[];
+var url_happy_image = "https://pbs.twimg.com/profile_images/562466745340817408/_nIu8KHX.jpeg"
 
-getToken (function () {
+var invoke = function () {
   $("img").each(function() {
-    //  console.log (this.src);
-
-    // if (sentimentAnalysis (this.src)) {
-    //
-    //   this.src = img;
-    //
-    // }
-
-    sentimentAnalysis (this.src, printTags);
-
-  })
-})
-
-
-
-
-function getToken (callback) {
-   var tokenAPI = "https://api.clarifai.com/v1/token/";
-
-   $.post (tokenAPI, {
-     client_id: "HwJ9xwDLs2aRbTKZ_qQoKQl-K0HyHqrL1333-Lsv",
-     client_secret: "1s-JTh4NXrS-PYe53MZFqQIsVQ7cOpIdwO_uHdAr",
-     grant_type: "client_credentials"
-   })
-    .done (function(data) {
-      console.log(data.access_token);
-      token = data.access_token;
-      callback();
+    var self = $(this);
+        
+    sentimentAnalysis (this.src, function(tags){
+        $.post('https://apiv2.indico.io/sentiment/batch?key=72a95c9f9ca2b62dc33fd2cbb29a5444',                
+               JSON.stringify({
+                'data': tags
+               })
+        ).then(function(res){
+            var myJson = JSON.parse(res)
+            console.log(myJson)
+            for(var i = 0; i<myJson.results.length; i++){
+                if(myJson.results[i] < 0.1){
+                    self.attr('src', url_happy_image)
+                }
+            }      
+        })
     })
+  })
+}()
+
+function dictionaryDef(tags, callback){
+    var newTags = []
+    for(var i = 0; i<tags.length; i++){
+            $.get("https://api.pearson.com/v2/dictionaries/ldoce5/entries?headword=" 
+                  + tags[i] + "&apikey=sAG888RoA39qSlaHQHztW8LWODfPQ1mm")
+                   .done (function(response) {
+                    console.log(response.results[0].senses[0].definition[0])
+                    console.log(typeof(response.results[0].senses[0].definition[0]))
+                    newTags.push(response.results[0].senses[0].definition[0])                     
+            }).done(function(){
+                callback(newTags)
+            })
+        }
 }
 
-function sentimentAnalysis (src,callback) {
-  var tagAPI = "https://api.clarifai.com/v1/tag/?url=" + src + "&access_token=" + token;
-  $.get (tagAPI,function (data) {
+function sentimentAnalysis (src, callback) {
+  var tagAPI = "https://api.clarifai.com/v1/tag/?url=" + src + "&access_token=" + "mH9JRdv5pg0cx8mw1gjUwqaMtuzM9B";
+  $.get (tagAPI, function (data) {
     //console.log(data);
-    for (var i = 0; i < data.results[0].result.tag.classes.length; i ++) {
+    var min = 10;
+    var tags = []
+    if(data.results[0].result.tag.classes.length < 10){
+        min = data.results[0].result.tag.classes.length
+    }
+    
+    for (var i = 0; i < min; i ++) {
       tags.push (data.results[0].result.tag.classes[i]);
     }
-    //console.log(tags);
-    printTags();
-    callback ();
-
+    
+    dictionaryDef(tags, callback);
+    //callback(tags)
   })
 }
 
-function printTags () {
-  console.log (tags);
-}
+
+
